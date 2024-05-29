ed4111f3c028c155e62c292af6c843edc6265433
Diego Viana <dtviana90@gmail.com>
2024-05-29 20:35:55 -0300

    feat: ajusta header da página

--- src/components/Careers.tsx
+++ src/components/Careers.tsx
@@ -1,12 +1,60 @@
 import React from 'react';
+import '../styles.css';
 
-const Careers: React.FC = () => {
+const Contact: React.FC = () => {
   return (
-    <div>
-      <h1>Trabalhe Conosco</h1>
-      <p>Estamos sempre em busca de novos talentos. Junte-se a nós!</p>
+    <div className="contact-container">
+      <div className="contact-form">
+        <h2>Trabalhe Conosco</h2>
+        <p>Utilize esse formulário para as tirar suas dúvidas, deixar sugestões e/ou fazer alguma reclamação.</p>
+        <p>Nossa equipe irá analisar e assim que possível, entraremos em contato!</p>
+        <form>
+  <div className="form-group">
+    <label htmlFor="name">Seu Nome</label>
+    <input type="text" id="name" name="name" />
+  </div>
+  <div className="form-group">
+    <label htmlFor="email">Seu Email</label>
+    <input type="email" id="email" name="email" />
+  </div>
+  <div className="form-group">
+    <label htmlFor="phone">Seu Telefone</label>
+    <input type="tel" id="phone" name="phone" />
+  </div>
+  <div className="form-group-flex">
+    <div className="form-group">
+      <label htmlFor="city">Cidade</label>
+      <input type="text" id="city" name="city" />
     </div>
+    <div className="form-group">
+      <label htmlFor="state">Estado</label>
+      <input type="text" id="state" name="state" />
+    </div>
+  </div>
+  <div className="form-group">
+    <label htmlFor="address">Endereço</label>
+    <input type="text" id="address" name="address" />
+  </div>
+  <div className="form-group">
+    <label htmlFor="subject">Assunto: </label>
+    <select id="subject" name="subject">
+      <option value="Opção 1">Locação simples</option>
+      <option value="Opção 2">Locação frota</option>
+      <option value="Opção 3">Reclamação/Sugestão</option>
+      <option value="Opção 4">Dúvida</option>
+      <option value="Opção 5">Elogio</option>
+    </select>
+  </div>
+  <div className="form-group">
+    <label htmlFor="message">Mensagem</label>
+    <textarea id="message" name="message"></textarea>
+  </div>
+  <button type="submit">Enviar</button>
+</form>
+      </div>
+      </div>
+
   );
 };
 
-export default Careers;
+export default Contact;
